f7e98392e7514da6fb638233c1a0c475c91fc415
heitor364 <criancas08@gmail.com>
2019-07-14 13:50:45 -0300

    codigo pronto 2.0

--- cn_heitor.py
+++ cn_heitor.py
@@ -4,10 +4,19 @@
 alfabeto = {' ':'@','!':'!','?':'?','.':'.',',':',',':':':',';':';','(':'(',')':')','a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'*10*','k':'*11*','l':'*12*','m':'*13*','n':'*14*','o':'*15*','p':'*16*','q':'*17*','r':'*18*','s':'*19*','t':'*20*','u':'*21*','v':'*22*','w':'*23*','y':'*25*','z':'*26*','x':'*27*','ç':'*28*','ã':'*30*','é':'*31*','ê':'*32*','í':'*33*','â':'*34*','á':'*35*','õ':'*36*','ó':'*37*','ú':'*38*','ù':'*39*' }
 
 codigo_pronto = ''
+flag = 0
 
-print('Digite a frase que quer  codificar sem ascentos ou caracteres especiais (@,#,$,%,\",\',\):')
-entrada = input().lower().strip()
-print(entrada)
+while flag == 0:
+
+    print('Digite a frase que quer  codificar sem caracteres especiais (@,#,$,%,\",\',\\):')
+    entrada = input().lower().strip()
+
+    if '@' in entrada or '#' in entrada or '$' in entrada or '%' in entrada or '"' in entrada or '\'' in entrada or '\\' in entrada or '/' in entrada:
+        print('Seu burro e cego não pode ter caracteres especiais (@,#,$,%,\",\',\\) na frase. Na proxima a MOMO vai te levar! \n')
+        entrada = ''
+    else:
+        flag = 1
+
 
 for i in range(len(entrada)):
 
