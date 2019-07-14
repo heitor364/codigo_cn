62fc682a4283126a4e770735a5defbf822e9b422
heitor364 <52861188+heitor364@users.noreply.github.com>
2019-07-13 22:10:38 -0300

    Update cn_heitor.py

--- cn_heitor.py
+++ cn_heitor.py
@@ -1,3 +1,4 @@
+# https://git-scm.com/book/pt-br/v1/Primeiros-passos-No%C3%A7%C3%B5es-B%C3%A1sicas-de-Git
 # Este é um progama para codificar em CN (Códogo Numerico)
 
 alfabeto = {'a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'*10*','k':'*11*','l':'*12*','m':'*13*','n':'*14*','o':'*15*','p':'*16*','q':'*17*','r':'*18*','s':'*19*','t':'*20*','u':'*21*','v':'*22*','w':'*23*','y':'*25*','z':'*26*','x':'*27*'}
